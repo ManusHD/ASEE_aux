6e3bc059e698c47f9aaeec270d2abcf7fa27034b
Manu <mhorrillj@alumnos.unex.es>
2023-12-15 14:53:49 +0100

    Test Borrar Evento

--- app/src/androidTest/java/es/unex/gps/weathevent/view/EventosTest.kt
+++ app/src/androidTest/java/es/unex/gps/weathevent/view/EventosTest.kt
@@ -29,90 +29,41 @@ class EventosTest {
     var mActivityScenarioRule = ActivityScenarioRule(IniciarSesionActivity::class.java)
 
     @Test
-    fun eventosTest() {
+    fun CrearEventoTest() {
         val materialButton = onView(
-            allOf(
-                withId(R.id.registro), withText("Registrarse"),
-                childAtPosition(
-                    allOf(
-                        withId(R.id.fondo),
-                        childAtPosition(
-                            withId(android.R.id.content),
-                            0
-                        )
-                    ),
-                    7
-                ),
-                isDisplayed()
-            )
+            withId(R.id.registro)
         )
         materialButton.perform(click())
 
         val appCompatEditText = onView(
-            allOf(
-                withId(R.id.nombreRegistro),
-                childAtPosition(
-                    childAtPosition(
-                        withId(android.R.id.content),
-                        0
-                    ),
-                    2
-                ),
-                isDisplayed()
-            )
+            withId(R.id.nombreRegistro)
         )
-        appCompatEditText.perform(replaceText("1"), closeSoftKeyboard())
+        appCompatEditText.perform(replaceText("test registro"), closeSoftKeyboard())
 
         val appCompatEditText2 = onView(
-            allOf(
-                withId(R.id.usernameRegistro),
-                childAtPosition(
-                    childAtPosition(
-                        withId(android.R.id.content),
-                        0
-                    ),
-                    4
-                ),
-                isDisplayed()
-            )
+            withId(R.id.usernameRegistro)
         )
-        appCompatEditText2.perform(replaceText("1"), closeSoftKeyboard())
+        appCompatEditText2.perform(replaceText("testCrearEvento"), closeSoftKeyboard())
 
         val appCompatEditText3 = onView(
-            allOf(
-                withId(R.id.emailRegistro),
-                childAtPosition(
-                    childAtPosition(
-                        withId(android.R.id.content),
-                        0
-                    ),
-                    6
-                ),
-                isDisplayed()
-            )
+            withId(R.id.emailRegistro)
         )
-        appCompatEditText3.perform(replaceText("1@1.es"), closeSoftKeyboard())
+        appCompatEditText3.perform(replaceText("test@test.es"), closeSoftKeyboard())
 
         val appCompatEditText4 = onView(
-            allOf(
-                withId(R.id.passRegistro),
-                childAtPosition(
-                    childAtPosition(
-                        withId(android.R.id.content),
-                        0
-                    ),
-                    8
-                ),
-                isDisplayed()
-            )
+            withId(R.id.passRegistro)
         )
-        appCompatEditText4.perform(replaceText("1"), closeSoftKeyboard())
+        appCompatEditText4.perform(replaceText("test123"), closeSoftKeyboard())
 
         val materialButton7 = onView(
             withId(R.id.confirmarRegistro)
         )
         materialButton7.perform(click())
 
+        val frameLayout = onView(
+            withId(R.id.bottom_navigation)
+        )
+        frameLayout.check(matches(isDisplayed()))
         val bottomNavigationItemView = onView(
             allOf(
                 withId(R.id.listEventFragment), withContentDescription("Eventos"),
@@ -261,6 +212,220 @@ class EventosTest {
             )
         )
         viewGroup.check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun borrarEventoTest() {
+        val materialButton = onView(
+            withId(R.id.registro)
+        )
+        materialButton.perform(click())
+
+        val appCompatEditText = onView(
+            withId(R.id.nombreRegistro)
+        )
+        appCompatEditText.perform(replaceText("test registro"), closeSoftKeyboard())
+
+        val appCompatEditText2 = onView(
+            withId(R.id.usernameRegistro)
+        )
+        appCompatEditText2.perform(replaceText("testBorrarEvento"), closeSoftKeyboard())
+
+        val appCompatEditText3 = onView(
+            withId(R.id.emailRegistro)
+        )
+        appCompatEditText3.perform(replaceText("test@test.es"), closeSoftKeyboard())
+
+        val appCompatEditText4 = onView(
+            withId(R.id.passRegistro)
+        )
+        appCompatEditText4.perform(replaceText("test123"), closeSoftKeyboard())
+
+        val materialButton7 = onView(
+            withId(R.id.confirmarRegistro)
+        )
+        materialButton7.perform(click())
+
+        val frameLayout = onView(
+            withId(R.id.bottom_navigation)
+        )
+        frameLayout.check(matches(isDisplayed()))
+        val bottomNavigationItemView = onView(
+            allOf(
+                withId(R.id.listEventFragment), withContentDescription("Eventos"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.bottom_navigation),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        bottomNavigationItemView.perform(click())
+
+        val floatingActionButton = onView(
+            allOf(
+                withId(R.id.add_event_button), withContentDescription("Añadir evento"),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.listEventFragment),
+                        childAtPosition(
+                            withId(R.id.nav_host_fragment),
+                            0
+                        )
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        floatingActionButton.perform(click())
+
+        val textInputEditText = onView(
+            allOf(
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.event_name_view),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        textInputEditText.perform(replaceText("Evento Test"), closeSoftKeyboard())
+
+        val textInputEditText2 = onView(
+            allOf(
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.location_view),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        textInputEditText2.perform(replaceText("La Coronada"), closeSoftKeyboard())
+
+        val materialButton2 = onView(
+            allOf(
+                withId(R.id.date_picker_btn), withText("Seleccionar fecha"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    4
+                ),
+                isDisplayed()
+            )
+        )
+        materialButton2.perform(click())
+
+        val materialButton3 = onView(
+            allOf(
+                withId(android.R.id.button1), withText("OK"),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("android.widget.ScrollView")),
+                        0
+                    ),
+                    3
+                )
+            )
+        )
+        materialButton3.perform(scrollTo(), click())
+
+        val materialButton4 = onView(
+            allOf(
+                withId(R.id.hour_picker_btn), withText("Seleccionar hora"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    6
+                ),
+                isDisplayed()
+            )
+        )
+        materialButton4.perform(click())
+
+        val materialButton5 = onView(
+            allOf(
+                withId(android.R.id.button1), withText("OK"),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("android.widget.ScrollView")),
+                        0
+                    ),
+                    3
+                )
+            )
+        )
+        materialButton5.perform(scrollTo(), click())
+
+        Thread.sleep(10000)
+
+        val materialButton6 = onView(
+            allOf(
+                withId(R.id.save_event_btn), withText("Guardar"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    8
+                ),
+                isDisplayed()
+            )
+        )
+        materialButton6.perform(click())
+
+        val viewGroup = onView(
+            allOf(
+                withParent(
+                    allOf(
+                        withId(R.id.rv_list_events),
+                        withParent(withId(R.id.listEventFragment))
+                    )
+                ),
+                isDisplayed()
+            )
+        )
+        viewGroup.check(matches(isDisplayed()))
+
+        val appCompatImageButton = onView(
+            allOf(
+                withId(R.id.delete_button), withContentDescription("Borrar evento"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.cv_event),
+                        0
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatImageButton.perform(click())
+
+        val viewGroup2 = onView(
+            allOf(
+                withParent(
+                    allOf(
+                        withId(R.id.rv_list_events),
+                        withParent(withId(R.id.listEventFragment))
+                    )
+                ),
+                isDisplayed()
+            )
+        )
+        viewGroup2.check(doesNotExist())
     }
 
     private fun childAtPosition(
