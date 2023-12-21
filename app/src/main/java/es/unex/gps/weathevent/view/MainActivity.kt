59747beb8d1f1f3c54ce9227ad0a4fcacb2b7e2f
jesuchito <jcarreterol2002@gmail.com>
2023-12-22 00:12:02 +0100

    a

--- app/src/main/java/es/unex/gps/weathevent/view/MainActivity.kt
+++ app/src/main/java/es/unex/gps/weathevent/view/MainActivity.kt
@@ -64,6 +64,7 @@ class MainActivity : AppCompatActivity(), UserParam, OnCiudadClickListener, OnCl
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
